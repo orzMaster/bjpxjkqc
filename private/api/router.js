818f77a0357c346961862a267d98b69940d5ba38
orzMaster <master@orzsoft.com>
2016-07-19 21:59:00 +0800

    update

--- private/api/router.js
+++ private/api/router.js
@@ -6,15 +6,6 @@ var router = express.Router();
 var controller = require('./controller');
 var upload = require('./upload');
 
-router.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-	res.header("X-Powered-By", ' 3.2.1')
-	res.header("Content-Type", "application/json;charset=utf-8");
-	next();
-});
-
 router.post('/reserve/delete', controller.reserve_delete);
 router.post('/reserve/update', controller.reserve_update);
 router.post('/reserve/list', controller.reserve_list);
